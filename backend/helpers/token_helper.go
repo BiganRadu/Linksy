498cd513806283380227ef961d36e19ccb3ece06
BiganRadu <radubigan5@gmail.com>
2024-10-20 18:04:21 +0300

    Add helpers for building jwt tokens.

---
+++ backend/helpers/token_helper.go
@@ -0,0 +1,48 @@
+package helpers
+
+import (
+	"backend/models"
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+type TokenHelper struct {
+	SecretKey string
+}
+
+func NewTokenHelper(secretKey string) *TokenHelper {
+	return &TokenHelper{
+		SecretKey: secretKey,
+	}
+}
+
+func (t *TokenHelper) GenerateToken(email, username string, createdAt int64) (string, error) {
+	claims := &models.MemberSignedDetails{
+		Email:     email,
+		Username:  username,
+		CreatedAt: createdAt,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+		},
+	}
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(t.SecretKey))
+	return token, err
+}
+
+func (t *TokenHelper) ValidateToken(tokenString string) (*models.MemberSignedDetails, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &models.MemberSignedDetails{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(t.SecretKey), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*models.MemberSignedDetails)
+	if !ok {
+		return nil, err
+	}
+
+	return claims, nil
+}
