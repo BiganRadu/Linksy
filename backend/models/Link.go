c5b3abff9430898e4da3e2f03e8c2f579ea15bbf
BiganRadu <radubigan5@gmail.com>
2025-03-01 01:49:52 +0200

    Add json structure tags for link

--- backend/models/Link.go
+++ backend/models/Link.go
@@ -1,15 +1,16 @@
 package models
 
 type Link struct {
-	ID             string        `bson:"_id"`
-	Title          string        `bson:"title"`
-	MemberEmail    string        `bson:"member_email"`
-	AccessMode     int           `bson:"access_mode"`
-	AllowedIps     []int32       `bson:"allowed_ips"`
-	BlackListedIps []int32       `bson:"black_listed_ips"`
-	AccessEntries  []AccessEntry `bson:"access_entries"`
-	ReferencedLink string        `bson:"referenced_link"`
-	CreatedAt      int64         `bson:"created_at"`
+	ID             string        `bson:"_id" json:"id"`
+	Title          string        `bson:"title" json:"title"`
+	Icon           string        `bson:"icon" json:"icon"`
+	MemberEmail    string        `bson:"member_email" json:"member_email"`
+	AccessMode     int           `bson:"access_mode" json:"access_mode"`
+	AllowedIps     []uint32      `bson:"allowed_ips" json:"allowed_ips"`
+	BlackListedIps []uint32      `bson:"black_listed_ips" json:"black_listed_ips"`
+	AccessEntries  []AccessEntry `bson:"access_entries" json:"access_entries"`
+	ReferencedLink string        `bson:"referenced_link" json:"referenced_link"`
+	CreatedAt      int64         `bson:"created_at" json:"created_at"`
 }
 
 type AccessEntry struct {
