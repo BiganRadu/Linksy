92e846e9983000528fc7ff8f05486a59c20df261
BiganRadu <radubigan5@gmail.com>
2025-03-01 02:02:26 +0200

    Change arguments of the cards and create the functionalities for share and deleting links

--- frontend/src/app/LinksPage.tsx
+++ frontend/src/app/LinksPage.tsx
@@ -6,12 +6,15 @@ import Stack from '@mui/material/Stack';
 import getSignInSideTheme from './theme/getSignInSideTheme';
 import { createTheme, ThemeProvider, PaletteMode } from '@mui/material/styles';
 import SideMenu from './SideMenu';
-import MainGrid from './MainGrid';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import CustomCard from './CustomCard';
-import { 
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import {
 	Button,
+	IconButton,
 	Typography,
 	Divider
  } from '@mui/material';
@@ -23,12 +26,45 @@ export default function Dashboard(props: { disableCustomTheme?: boolean }) {
 	const [email, setEmail] = React.useState('');
 	const [links, setLinks] = React.useState([]);
 	const [linksError, setLinksError] = React.useState('');
+	const [confirmation, setConfirmation] = React.useState(false);
+	const [corfirmationId, setConfirmationId] = React.useState('');
+	const [shareDialog, setShareDialog] = React.useState(false);
 	const SignInSideTheme = createTheme(getSignInSideTheme(mode));
-	// This code only runs on the client side, to determine the system color preference
-	React.useEffect(() => {
-	  // Check if there is a preferred mode in localStorage
-	const fetchMemberInfo = async () => {
 
+	const redirectToCreateLink = () => {
+		window.location.href = '/app/create-link';
+	}
+
+	const redirectToEditLink = (id: string) => {
+		window.location.href = `/app/edit-link/${id}`;
+	}
+
+	const handleDelete = (id: string) => {
+		setConfirmation(true);
+		setConfirmationId(id);
+	}
+
+	const handleShare = () => {
+		setShareDialog(true);
+	}
+
+	const submitDelete = () => {
+		const authToken = Cookies.get('AuthToken');
+		axios.delete(`http://localhost:3000/app/delete-link?link_id=${corfirmationId}`, {
+			headers: {
+				AuthToken: authToken,
+			},
+		}).then(response => {
+			console.log(response);
+			window.location.reload();
+		}).catch(error => {
+			console.log(error);
+		})
+	}
+
+	React.useEffect(() => {
+
+	const fetchMemberInfo = async () => {
 		const authToken = Cookies.get('AuthToken');
 		axios.get('http://localhost:3000/app/member-info', {
 			headers: {
@@ -50,8 +86,6 @@ export default function Dashboard(props: { disableCustomTheme?: boolean }) {
 				AuthToken: authToken,
 			},
 		}).then(response => {
-			console.log(response);
-			console.log("aici");
 			setLinks(response.data.links);
 		}).catch(error => {
 			setLinksError('Could not fetch links');
@@ -60,17 +94,32 @@ export default function Dashboard(props: { disableCustomTheme?: boolean }) {
 
 	fetchMemberInfo();
 	fetchMemberLinks();
+
 	  const systemPrefersDark = window.matchMedia(
 		  '(prefers-color-scheme: dark)',
 		).matches;
-		setMode(systemPrefersDark ? 'dark' : 'light');
-	  }, []);
+	setMode(systemPrefersDark ? 'dark' : 'light');
+
+	}, []);
+
+	React.useEffect(() => {
+		if (confirmation || shareDialog) {
+			document.body.style.overflow = 'hidden'; // Disable scrolling
+		} else {
+			document.body.style.overflow = 'auto'; // Enable scrolling
+		}
+	
+		// Cleanup function to restore scrolling when component unmounts or state changes
+		return () => {
+			document.body.style.overflow = 'auto';
+		};
+	}, [confirmation, shareDialog]);
+
   return (
     <ThemeProvider theme={SignInSideTheme}>
       <CssBaseline enableColorScheme />
       <Box sx={{ display: 'flex' }}>
-        {/* Main content */}
-		<SideMenu username={username} email={email}/>
+		<SideMenu username={username} email={email} selectedItem='Links'/>
         <Box
           component="main"
           sx={(theme) => ({
@@ -94,21 +143,154 @@ export default function Dashboard(props: { disableCustomTheme?: boolean }) {
 				<Typography variant="h3" component="div">
 					Links Page
 				</Typography>
-				<Button variant="contained" size="small">
+				<Button variant="contained" size="small" onClick={redirectToCreateLink}>
 					Create Link
 				</Button>
 			</Box>
 			<Divider />
-			{links === undefined ? (
+			{links === undefined || links === null ? (
 				<p>No links available</p>
 			) : (
 				links.map((link, index) => (
-					<CustomCard key={index} title={link.title} link_id={link.id} original_url={link.referenced_link} />
+					<CustomCard 
+						key={index} 
+						title={link.title} 
+						icon_url={link.icon} 
+						link_id={link.id} 
+						original_url={link.referenced_link} 
+						created_at={link.created_at} 
+						onEdit={() => redirectToEditLink(link.id)}
+						onDelete={() => handleDelete(link.id)}
+						onShare={handleShare}
+					/>
 				))
 			)}
           </Stack>
         </Box>
       </Box>
+	{confirmation && (
+		<Box
+			sx={{
+				position: 'fixed',
+				top: 0,
+				left: 0,
+				width: '100%',
+				height: '100%',
+				backgroundColor: 'rgba(0, 0, 0, 0.5)',
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				zIndex: 1300,
+			}}
+		>
+			<Box
+				sx={{
+					backgroundColor: 'blue',
+					padding: 4,
+					borderRadius: 2,
+					boxShadow: 24,
+					textAlign: 'center',
+				}}
+			>
+				<Typography variant="h6" component="div" gutterBottom>
+					Are you sure you want to delete this link?
+				</Typography>
+				<Button
+					variant="contained"
+					color="primary"
+					onClick={() => {
+						submitDelete();
+						setConfirmation(false);
+						setConfirmationId('');
+					}}
+					sx={{ mr: 2 }}
+				>
+					Yes
+				</Button>
+				<Button
+					variant="outlined"
+					color="secondary"
+					onClick={() => {
+						setConfirmation(false)
+						setConfirmationId('');
+					}}
+				>
+					No
+				</Button>
+			</Box>
+		</Box>
+	)}
+	{shareDialog && (
+		<Box
+			sx={{
+				position: 'fixed',
+				top: 0,
+				left: 0,
+				width: '100%',
+				height: '100%',
+				backgroundColor: 'rgba(0, 0, 0, 0.5)',
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				zIndex: 1300,
+			}}
+		>
+			<Box
+				sx={{
+					backgroundColor: 'blue',
+					padding: 4,
+					borderRadius: 2,
+					boxShadow: 24,
+					position: 'relative',
+					textAlign: 'center',
+				}}
+			>
+				<Button
+					sx={{
+						position: 'absolute',
+						top: 8,
+						right: 8,
+						backgroundColor: 'transparent',
+						'&:hover': {
+							backgroundColor: 'transparent',
+						},
+						'&:focus': {
+							outline: 'none',
+						},
+					}}
+					onClick={() => setShareDialog(false)}
+				>
+					X
+				</Button>
+				<Typography variant="h6" component="div" gutterBottom>
+					Share this link
+				</Typography>
+				<Box sx={{ display: 'flex', justifyContent: 'center', gap: 2 }}>
+					<IconButton
+						color="primary"
+						href="https://www.facebook.com/sharer/sharer.php?u=YOUR_LINK_HERE"
+						target="_blank"
+					>
+						<FacebookIcon />
+					</IconButton>
+					<IconButton
+						color="primary"
+						href="https://twitter.com/intent/tweet?url=YOUR_LINK_HERE"
+						target="_blank"
+					>
+						<TwitterIcon />
+					</IconButton>
+					<IconButton
+						color="primary"
+						href="https://www.linkedin.com/shareArticle?mini=true&url=YOUR_LINK_HERE"
+						target="_blank"
+					>
+						<LinkedInIcon />
+					</IconButton>
+				</Box>
+			</Box>
+		</Box>
+	)}
     </ThemeProvider>
   );
 }
